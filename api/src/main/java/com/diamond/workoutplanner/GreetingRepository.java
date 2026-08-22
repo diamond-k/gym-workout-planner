6081dcfa131a7331cbc0b8bd36cbf56f8a13e0ee
Diamond <mzladylush@googlemail.com>
2026-08-22 16:30:28 +0100

    Remove greeting starter and add API plan

--- api/src/main/java/com/diamond/workoutplanner/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diamond.workoutplanner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepository extends JpaRepository<Greeting, Integer> {
-    // Spring Boot automatically implements this at runtime!
-}
--- api/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO greetings (message)
-SELECT 'Hello World from Spring Boot Seed!'
-WHERE NOT EXISTS (SELECT 1 FROM greetings);
